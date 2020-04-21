a40bd2b35057181c0420a4797b041c1f323fd0ef
pauumoreira <61234910+pauumoreira@users.noreply.github.com>
2020-04-21 20:22:56 -0300

    Form datos

--- app.py
+++ app.py
@@ -27,7 +27,6 @@ def Index():
     return render_template('index.html')
 
 #Registro
-
 class RegisterForm(Form):
     email = StringField('Correo', [validators.Length(min=6, max=50)])
     password = PasswordField('Contraseña', [
@@ -42,7 +41,6 @@ def registro():
     if request.method == 'POST' and form.validate():
         email = form.email.data
         password = sha256_crypt.encrypt(str(form.password.data))
-
         cur = mysql.get_db().cursor()
         cur.execute("INSERT INTO Usuarios(email, contraseña_encriptada) VALUES(%s, %s)",(email, password))
         mysql.get_db().commit()
@@ -50,18 +48,54 @@ def registro():
 
         flash('¡Ya estás registrado!', 'success')
         return redirect(url_for('datos'))
-
     return render_template('registro.html', form=form)
-
-
 app.config['SECRET_KEY'] = 'any secret string'
 
-
 #Ingresar datos
-@app.route('/datos')
+class DatosForm(Form):
+    nombre = StringField('Nombre/s', [validators.Length(min=1, max=50)])
+    apellido = StringField('Apellido/s', [validators.Length(min=1, max=50)])
+    fecha_nacimiento = StringField('Fecha de Nacimiento', [validators.Length(min=1, max=50)])
+    celular = StringField('Celular', [validators.Length(min=1, max=50)])
+    telefono = StringField('Teléfono', [validators.Length(min=1, max=50)])
+    pais = StringField('País', [validators.Length(min=1, max=50)])
+    ciudad = StringField('Ciudad', [validators.Length(min=1, max=50)])
+    profesion = StringField('Profesión', [validators.Length(min=1, max=50)])
+    empresa = StringField('Empresa', [validators.Length(min=1, max=50)])
+    cargo = StringField('Cargo', [validators.Length(min=1, max=50)])
+    banco = StringField('Banco', [validators.Length(min=1, max=50)])
+    num_cuenta = StringField('Numero de cuenta', [validators.Length(min=1, max=50)])
+    redes_sociales = StringField('Redes sociales', [validators.Length(min=1, max=50)])
+
+@app.route('/datos', methods = ['POST', 'GET'])
 def datos():
-    #usuarioEmail = request.form['mail']
-    return render_template('ingresarDatos.html')
+    form = DatosForm(request.form)
+    if request.method == 'POST' and form.validate():
+        nombre = form.nombre.data
+        apellido = form.apellido.data
+        fecha_nacimiento = form.fecha_nacimiento.data
+        celular = form.celular.data
+        telefono = form.telefono.data
+        pais = form.pais.data
+        ciudad = form.ciudad.data
+        profesion = form.profesion.data
+        empresa = form.empresa.data
+        cargo = form.cargo.data
+        banco = form.banco.data
+        num_cuenta = form.num_cuenta.data
+        redes_sociales = form.redes_sociales.data
+
+        cur = mysql.get_db().cursor()
+        cur.execute("INSERT INTO datos(nombre, apellido, fecha_nacimiento, celular, telefono, pais, ciudad, profesion, empresa, cargo, banco, num_cuenta, redes_sociales) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(nombre, apellido, fecha_nacimiento, celular, telefono, pais, ciudad, profesion, empresa, cargo, banco, num_cuenta, redes_sociales))
+        mysql.get_db().commit()
+        cur.close()
+
+        flash('¡Datos completos!', 'success')
+        return redirect(url_for('Index'))
+    return render_template('ingresarDatos.html', form=form)
+
+
+
 #Login
 @app.route('/inicio', methods = ['POST', 'GET'])
 def ingresar():
